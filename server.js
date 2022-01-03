6f0cec3cd527ee6594b0dd9f7b3aacfd902766a1
RaV <Rafal193zm@gmail.com>
2022-01-03 10:56:18 +0100

    Object

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get("/", function (req, res) {
     res.send("<h1>my app on heroku! - small change</h1>")
 })
 
+app.get("/data", function (req, res) {
+    res.send({ Imie: "Rafał", Nazwisko: "Żmija", Klasa: "3i2", Grupa: "2" })
+})
+
 app.listen(PORT, function () {
     console.log("start serwera na porcie " + PORT)
 })
